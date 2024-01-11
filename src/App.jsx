59658fa734e50255222200f34fa80b67e1fda1c2
Alejandro Cadena <alejandro.cadena@sms-group.com>
2024-01-11 11:37:52 -0500

    listado de criptomonedas usando fetch > CoinCap api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import loading from "../public/Loading.gif"
 
 function App() {
 
+  const [criptos, setCriptos] = useState()
+
+  useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets")
+     .then((resp) => resp.json())
+     .then((data) => {setCriptos(data.data)})
+     .catch(() => {console.error("La peticion fallo")})
+  }, [])
+
+  // Loader
+  if (!criptos) return <img src={loading} alt='Cargando ....' />
+
+
   return (
     <>
-      <h1>Section Six Router App</h1>
+      <h1>Lista de Criptomonedas</h1>
+      <ol>
+        { criptos.map(({name, priceUsd}) => (
+          <li key={name}>Nombre {name} con un precio de: ${priceUsd}</li>
+        ))}
+      </ol>
     </>
   )
 }
